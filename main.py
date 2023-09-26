58053601f8ecafaea7bf31f32a1f48abbdffdb53
Surya Pratap Singh Naruka <52036523+suryapratapsinghnaruka@users.noreply.github.com>
2023-09-27 01:40:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -303,4 +303,4 @@ def get_pdf():
     return send_from_directory(directory, os.path.basename(pdf_path), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
